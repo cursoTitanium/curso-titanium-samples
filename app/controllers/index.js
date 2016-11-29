9e477e6efe743688dd384f92f30f9d62fc7da3a9
Jorge Macías <jormagar@upvnet.upv.es>
2016-11-29 23:08:09 +0100

    ActionBar

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,2 +1,17 @@
+function changeSearch(e){
+	alert($.searchView.value);
+}
+
+function refreshAction(e){
+	alert("Click en refresh!");
+}
+
+function shareAction(e){
+	alert("Click en share!");
+}
+
+function moreAction(e){
+	alert("Click en más acciones!");
+}
 
 $.index.open();
